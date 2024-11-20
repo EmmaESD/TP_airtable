5107bf6ddceb5482c32becc1dd277806b7b06344
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-11-20 15:55:40 +0100

    init project

---
+++ src/utils/types/client.ts
@@ -0,0 +1,23 @@
+export enum Status {
+  CONTACTED = "contacted",
+  NOT_CONTACTED = "not contacted",
+  CONTACT_IN_FUTUR = "contact in futur",
+}
+
+export type Client = {
+  id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone_number: string;
+  notes: string;
+  status: Status;
+};
+
+export type Clients = Client[];
+export type ClientDTO = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone_number: string;
+};
